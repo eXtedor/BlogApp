61fc4aecf4e083b26b8987c94f51493aaf159ac7
eXtedor <43319432+eXtedor@users.noreply.github.com>
2018-11-25 15:56:59 +0100

    LoginRegContainer, VM üres

--- BlogApp/BlogApp/View/LoginRegContainer.xaml.cs
+++ BlogApp/BlogApp/View/LoginRegContainer.xaml.cs
@@ -15,13 +15,18 @@ using System.Windows.Shapes;
 namespace BlogApp.View
 {
     /// <summary>
-    /// Interaction logic for ContainerWindow.xaml
+    /// Interaction logic for LoginRegContainer.xaml
     /// </summary>
-    public partial class ContainerWindow : Window
+    public partial class LoginRegContainer : Window
     {
-        public ContainerWindow()
+        Style Primary = Application.Current.FindResource("MaterialDesignRaisedButton") as Style;
+        Style Secondary = Application.Current.FindResource("MaterialDesignRaisedDarkButton") as Style;
+        public LoginRegContainer()
         {
             InitializeComponent();
+            Main.Content = new LoginPage();
+            signin.Style = Primary;
+            signup.Style = Secondary;
         }
     }
 }
