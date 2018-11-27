ac2f7d75f4f2bc668963ea346d1ab842142b0817
eXtedor <43319432+eXtedor@users.noreply.github.com>
2018-11-27 22:13:23 +0100

    EventAggregator, PostActions, MainContainerWindow viewmodel

---
+++ BlogApp/BlogApp/BusinessLogic/PostActions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogApp.Model;
+
+namespace BlogApp.BusinessLogic
+{
+    public static class PostActions
+    {
+        public static void AddPost(string body, int user_id)
+        {
+            using (var context = new BlogDbEntities())
+            {
+                Post newPost = new Post();
+                newPost.Body = body;
+                newPost.User_Id = user_id;
+                newPost.Created_At = DateTime.Now;
+                context.Post.Add(newPost);
+                context.SaveChanges();
+            }
+        }
+        public static void ModifyPost(int id, string body)
+        {
+            using (var context = new BlogDbEntities())
+            {
+                Post modifyThis = context.Post.Find(id);
+                modifyThis.Body = body;
+                modifyThis.Modified_At = DateTime.Now;
+                context.Entry(modifyThis).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public static void DeletePost(int id)
+        {
+            using (var context = new BlogDbEntities())
+            {
+                Post deleteThis = context.Post.Find(id);
+                context.Post.Remove(deleteThis);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ BlogApp/BlogApp/ViewModel/MainContainerWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using BlogApp.BusinessLogic;
+using BlogApp.Command;
+using BlogApp.UI;
+using BlogApp.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BlogApp.ViewModel
+{
+    class MainContainerWindowViewModel : ObservableObject
+    {
+        public int UserId
+        {
+            get { return UIRepository.Instance.CurrentClientId; }
+        }
+        public void UserLogout()
+        {
+            LoginLogout.UserLogout();
+            new LoginRegContainer().Show();
+            Application.Current.Windows[0].Close();
+        }
+        private ICommand _Logout;
+        public ICommand Logout
+        {
+            get
+            {
+                if (_Logout == null)
+                {
+                    _Logout = new RelayCommand(p => true, p => UserLogout());
+                }
+                return _Logout;
+            }
+        }
+        private string _PostBody;
+        public string PostBody
+        {
+            set
+            {
+                this._PostBody = value;
+            }
+            get
+            {
+                return _PostBody;
+            }
+        }
+        private ICommand _AddPost;
+        public ICommand AddPost
+        {
+            get
+            {
+                if (_AddPost == null)
+                {
+                    _AddPost = new RelayCommand(p => true, p => AddNewPost(PostBody, UserId));
+                }
+                return _AddPost;
+            }
+        }
+        public void AddNewPost(string body, int userid)
+        {
+            PostActions.AddPost(body, userid);
+            EventAggregator.BroadCast("Posts changed");
+        }
+    }
+}
